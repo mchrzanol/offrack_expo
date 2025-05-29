8d9268a708b7d74305b864d8bd884a6162b8f287
mchrzanol <mchrzanol22@gmail.com>
2025-05-29 18:57:21 +0200

    feat: repair tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,3 @@
-import '../global.css';
-
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -11,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-xl">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
